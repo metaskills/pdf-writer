b1ea6e98249ab7136986201ecb774e28bb49a1c8
sandal <sandal@8d9166c5-6811-0410-8782-ea71c07d3dbd>
2007-12-10 02:36:22 +0000

    tests for numbering, or a start on them

---
+++ test/test_page_numbering.rb
@@ -0,0 +1,13 @@
+require "test/helper"
+
+describe "Page Numbering" do
+  
+  before :each do 
+    @pdf = PDF::Writer.new
+  end
+  
+  it "should start with a current page = 1" do
+    @pdf.current_page_number.should == 1
+  end
+  
+end
